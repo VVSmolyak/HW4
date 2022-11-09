5dd0c29c8dffe6a9c5a561ee59c1c1ce3392e6ce
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2022-11-10 00:45:11 +0300

    task1 added

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Использование функций обязательно, начиная с этого ДЗ!
+
+//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+Console.WriteLine ("Введите число A: ");
+int A = int.Parse (Console.ReadLine()!);
+Console.WriteLine ("Введите число B: ");
+int B = int.Parse (Console.ReadLine()!);
+RaseAToPowerB (A,B);
+
+int RaseAToPowerB (int A, int B) {
+
+    int power = (int)(Math.Pow (A,B));
+    Console.WriteLine (power);
+    return power;
+}
+
+
