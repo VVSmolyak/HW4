116735d1c1f12f90b802216944682b3fbbcfcfa6
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2022-11-10 01:35:00 +0300

    task2 added

---
+++ task2_/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+Console.WriteLine("Введите число:");
+int Number = int.Parse(Console.ReadLine()!);
+OutputDigitSum(Number);
+
+int OutputDigitSum(int Number)
+{
+
+    int Sum = 0;
+    while (Number > 0)
+    {
+        Sum = Sum + Number % 10;
+        Number = Number / 10;
+    }
+
+    Console.WriteLine ("Сумма цифр в числе: " + Sum);
+    return Sum;
+}
+
+
