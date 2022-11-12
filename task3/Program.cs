25bb82cda608185255d9aa92fa112b533434a7d9
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2022-11-12 23:59:09 +0300

    task3 added

---
+++ task3/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] Array = new int[8];
+FillArray (Array);
+PrintArray (Array);
+
+
+void FillArray(int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine ("Введите число:");
+        Array[index] = int.Parse (Console.ReadLine ()!);
+        index++;
+    }
+}
+
+void PrintArray(int[] Array)
+{
+    Console.Write("Массив:[ ");
+    int count = Array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(Array[position] + " ");
+        position++;
+    }
+    Console.WriteLine("]");
+}
+
